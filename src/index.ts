cd5b5f0121ec762287dca100dc52b88fcb8de033
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-11-13 11:33:57 +0100

    more tests and bugfixes

--- src/index.ts
+++ src/index.ts
@@ -7,21 +7,23 @@ import path = require('path');
  * @param cwd the absolute path of the current directory with forward slash or backslash as directory separator.
  */
 export function* normalizeGlob(input: string, cwd: string) {
+    if (input === '')
+        input = '.';
     for (let pattern of expandBraces(input)) {
         const trailingSlash = pattern.endsWith('/');
         const ing = isNegatedGlob(pattern);
-        pattern = ing.pattern;
+        ({pattern} = ing);
         let {root} = path.parse(pattern);
         let parts;
         if (root) {
             parts = pattern.substr(root.length).split(/\/+/g);
-            if (root.endsWith('/'))
-                root = root.slice(0, -1);
         } else {
             ({root} = path.parse(cwd));
+            if (!root)
+                throw new Error("'cwd' must be an absolute path");
             parts = [...cwd.substr(root.length).split(/[/\\]+/g), ...pattern.split(/\/+/g)];
-            if (/[/\\]$/.test(root))
-                root = root.slice(0, -1);
+            if (root.endsWith('\\'))
+                root = root.slice(0, -1) + '/';
         }
         if (ing.negated)
             root = '!' + root;
@@ -45,8 +47,16 @@ function* appendPath(result: string[], parts: string[], i: number, prefix: strin
                 result.push(parts[i]);
         }
     }
-    result.unshift(prefix);
-    if (trailingSlash)
-        result.push('');
-    yield result.join('/');
+    const joined = join(prefix, result.join('/'));
+    yield trailingSlash ? ensureTrailingSlash(joined) : joined;
+}
+
+function join(prefix: string, suffix: string) {
+    if (!suffix)
+        return prefix;
+    return ensureTrailingSlash(prefix) + suffix;
+}
+
+function ensureTrailingSlash(p: string) {
+    return p.endsWith('/') ? p : p + '/';
 }
--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -7,4 +7,18 @@ test('expands curly braces', (t) => {
 
 test('resolves individual globs', (t) => {
     t.deepEqual(Array.from(normalizeGlob('{/foo,bar}', '/baz')), ['/foo', '/baz/bar']);
+    t.deepEqual(Array.from(normalizeGlob('{/foo,bar}', '/')), ['/foo', '/bar']);
+});
+
+test('resolves . and ..', (t) => {
+    t.deepEqual(Array.from(normalizeGlob('..', '/baz/bar')), ['/baz']);
+    t.deepEqual(Array.from(normalizeGlob('../..', '/baz/bar')), ['/']);
+    t.deepEqual(Array.from(normalizeGlob('../../..', '/baz/bar')), ['/']);
+    t.deepEqual(Array.from(normalizeGlob('/baz/bar/..', '/')), ['/baz']);
+    t.deepEqual(Array.from(normalizeGlob('/baz/bar/../..', '/')), ['/']);
+    t.deepEqual(Array.from(normalizeGlob('/baz/bar/../../..', '/')), ['/']);
+    t.deepEqual(Array.from(normalizeGlob('.', '/')), ['/']);
+    t.deepEqual(Array.from(normalizeGlob('.', '/baz')), ['/baz']);
+    t.deepEqual(Array.from(normalizeGlob('./', '/baz')), ['/baz/']);
+    t.deepEqual(Array.from(normalizeGlob('', '/baz')), ['/baz']);
 });
